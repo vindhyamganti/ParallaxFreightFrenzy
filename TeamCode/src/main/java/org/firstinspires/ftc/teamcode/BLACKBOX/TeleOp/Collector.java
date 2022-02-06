5b50498a89b0fcb396b4e2ceb099d738b5627a6a
PrathikI <iyengar.prathik@gmail.com>
2022-02-06 18:05:09 -0500

    File Modifications

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLACKBOX/TeleOp/Collector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLACKBOX/TeleOp/Collector.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.BLACKBOX;
+package org.firstinspires.ftc.teamcode.BLACKBOX.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLACKBOX/TeleOp/HardwareRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLACKBOX/TeleOp/HardwareRobot.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.BLACKBOX;
+package org.firstinspires.ftc.teamcode.BLACKBOX.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
